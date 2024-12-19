f4a905b02a3a579f0433d2df30f0d5ff959d4bde
Thomas Lang <thomaslang2003@me.com>
2024-12-19 11:30:22 -0600

    Added button support, UNTESTED

--- src/main.c
+++ src/main.c
@@ -20,7 +20,7 @@ LOG_MODULE_REGISTER(main);
 #include <zephyr/sys/util.h>
 #include <zephyr/drivers/gpio.h>
 
-
+// Define Devicetree Aliases
 #define STRIP_NODE		DT_ALIAS(led_strip)
 
 #if DT_NODE_HAS_PROP(DT_ALIAS(led_strip), chain_length)
@@ -32,8 +32,16 @@ LOG_MODULE_REGISTER(main);
 #define CONFIG_SAMPLE_LED_UPDATE_DELAY 50
 #define DELAY_TIME K_MSEC(CONFIG_SAMPLE_LED_UPDATE_DELAY)
 
-// #define LED_ENABLE DT_ALIAS(led_enable)
+#define LED_ENABLE DT_ALIAS(led_enable)
+#define BUTTON0 DT_ALIAS(sw0)
 
+static const struct device *const strip = DEVICE_DT_GET(STRIP_NODE);
+static const struct gpio_dt_spec enable = GPIO_DT_SPEC_GET(LED_ENABLE, gpios);
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(BUTTON0, gpios, {0});
+// static const struct gpio_dt_spec enable =
+// 	GPIO_DT_SPEC_GET_OR(DT_NODELABEL(led_enable), gpios, {0});
+
+// Create RGB Colors
 #define RGB(_r, _g, _b) { .r = (_r), .g = (_g), .b = (_b) }
 
 static const struct led_rgb colors[] = {
@@ -42,28 +50,71 @@ static const struct led_rgb colors[] = {
 	RGB(0x00, 0x00, 0x00), /* blue */
 };
 
+static struct gpio_callback button_cb_data;
+
 static struct led_rgb pixels[STRIP_NUM_PIXELS];
 
-static const struct device *const strip = DEVICE_DT_GET(STRIP_NODE);
-// static const struct gpio_dt_spec enable = GPIO_DT_SPEC_GET(LED_ENABLE, gpios);
-static const struct gpio_dt_spec enable =
-	GPIO_DT_SPEC_GET_OR(DT_NODELABEL(led_enable), gpios, {0});
+enum STATE {
+	OFF, FIXED, BLUETOOTH, WAVE, IMU
+};
+
+volatile enum STATE currentState = FIXED;
+
+
+static enum STATE nextState(enum STATE currentState) {
+	switch(currentState){
+		case OFF:
+			return FIXED;
+			break;
+		case FIXED:
+			return BLUETOOTH;
+			break;
+		case BLUETOOTH:
+			return WAVE;
+			break;
+		case WAVE:
+			return IMU;
+			break;
+		case IMU:
+			return OFF;
+			break;
+	}
+	return FIXED;
+}
+
+void buttonPressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins) {
+	currentState = nextState(currentState);
+}
 
 int main(void)
 {
-	size_t color = 0;
-	int rc;
+	int ret;
 
 	if (!gpio_is_ready_dt(&enable)) {
 		printf("LED enable is not ready");
 		return 0;
 	}
 
-	int ret = gpio_pin_configure_dt(&enable, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
+	ret = gpio_pin_configure_dt(&enable, GPIO_OUTPUT_ACTIVE);
+	if (ret) {
 		printf("LED is not configured");
 		return 0;
 	}
+
+	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+	if (ret) {
+		printk("Error %d: failed to configure button", ret);
+		return 0;
+	}
+
+	ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
+	if (ret) {
+		printf("Could not configure sw0 GPIO interrupt (%d)\n", ret);
+		return 0;
+	}
+	gpio_init_callback(&button_cb_data, buttonPressed, BIT(button.pin));
+	gpio_add_callback(button.port, &button_cb_data);
+	printk("Set up button at %s pin %d\n", button.port->name, button.pin);
 
 	if (device_is_ready(strip)) {
 		printf("Found LED strip device %s", strip->name);
@@ -71,31 +122,29 @@ int main(void)
 		printf("LED strip device %s is not ready", strip->name);
 		return 0;
 	}
-	// printf(DT_PROP(DT_ALIAS(led_strip), delay-t1h));
-	printf("Test");
+
+
 	printf("Displaying pattern on strip");
 	while (1) {
-		// for (size_t cursor = 0; cursor < ARRAY_SIZE(pixels); cursor++) {
-		// 	memset(&pixels, 0x00, sizeof(pixels));
-		// 	memcpy(&pixels[cursor], &colors[color], sizeof(struct led_rgb));
-
-		// 	rc = led_strip_update_rgb(strip, pixels, STRIP_NUM_PIXELS);
-		// 	if (rc) {
-		// 		printf("couldn't update strip: %d", rc);
-		// 	}
-
-		// 	k_sleep(DELAY_TIME);
-		// }
-
-		// color = (color + 1) % ARRAY_SIZE(colors);
-
+		switch(currentState) {
+			case OFF:
+				break;
+			case FIXED:
+				break;
+			case BLUETOOTH:
+				break;
+			case WAVE:
+				break;
+			case IMU:
+				break;
+		}
 		memset(&pixels, 0x00, sizeof(pixels));
 		for (size_t cursor = 0; cursor < ARRAY_SIZE(pixels); cursor++) {
 			memcpy(&pixels[cursor], &colors[1], sizeof(struct led_rgb));
 		}
-		rc = led_strip_update_rgb(strip, pixels, STRIP_NUM_PIXELS);
-		if (rc) {
-			printf("couldn't update strip: %d", rc);
+		ret = led_strip_update_rgb(strip, pixels, STRIP_NUM_PIXELS);
+		if (ret) {
+			printf("couldn't update strip: %d", ret);
 		}
 
 		k_sleep(DELAY_TIME);
